53f016d6a362a71eca746a492836daa4d42fc08a
Andy Mabbutt <andy@pontbren.com>
2020-11-10 10:37:21 +0000

    covid update

--- src/covid.php
+++ src/covid.php
@@ -38,45 +38,108 @@ $page = basename($_SERVER["PHP_SELF"]);
 
 <h2 class="page-title">Covid-19</h2>
 
-<!-- <?php include("covid/local.php"); ?>Help -->
-<!-- <?php include("covid/help.php"); ?>Help -->
-<!-- <?php include("covid/business.php"); ?>Business -->
-<!-- <?php include("covid/education.php"); ?>Education & Families -->
-<!-- <?php include("covid/health.php"); ?>Health -->
-<!-- <?php include("covid/housing.php"); ?>housing -->
+<div class="bb mb5">
+  <h3>General Support</h3>
+  <h4>Welsh  Government</h4>
+  <p>View  the latest <a href="https://gov.wales/coronavirus">Welsh  Government Coronavirus (COVID-19)</a> guidance and regulations.</p>
+  <p>
+  </p>
+  <p>The  Welsh Government issues coronavirus bulletins weekly. You can sign up  to receive them at their News and Alerts page or email <a href="mailto:cabinetcommunications@gov.wales">cabinetcommunications@gov.wales</a></p>
+  <p>
+  </p>
+</div>
 
-<div data-accordion="1">
-  <details>
-  <summary>Local Information</summary> 
-      <?php include("covid/local.php"); ?>
-  </details>
 
-  <details>
-    <summary>Help & Information</summary> 
-      <?php include("covid/help.php"); ?>
-  </details>
+<div class="bb mb5">
+  <h4>Public  Health Wales</h4>
+  <p>View  the latest <a href="https://phw.nhs.wales/topics/latest-information-on-novel-coronavirus-covid-19/">coronavirus  statistics</a> and <a href="https://gov.wales/test-trace-protect-coronavirus">apply  to take a test</a> or <a href="https://gov.wales/safe-help/volunteering">volunteer  to help</a>.</p>
+  <p>
+  </p>
+  <h4><a href="https://www.ageuk.org.uk/cymru/">Age  Cymru – Friend in Need Service</a></h4>
+  <p>Find  information, reassurance and support across Wales.</p>
+  <p>
+  </p>
+  <h4><a href="https://newsroom.carmarthenshire.gov.wales/2020/10/kcs-covid19/keep-carmarthenshire-safe/#.X6lMmWj7Ryw">Carmarthenshire  County Council</a></h4>
+  <p>View  the latest news and information in your area and find support to  access funding or services. </p>
+  <p>
+  </p>
+  <h4><a href="https://llandeilocovid19.com/">Llandeilo  Covid-19 Community Support Group</a></h4>
+  <p>Find  support to access services and volunteer to help in the Llandeilo and  Upper Towy Valley area.</p>
+  <p>
+  </p>
+  <h4>Salem  Covid-19 Mutual Aid Group</h4>
+  <p>Find  support to collect prescriptions, shopping, or have a chat in the  Salem area.</p>
+  <p>Contact  us at: </p>
+  <p>Facebook:  <a href="https://www.facebook.com/groups/SalemMutualAidGroup/">www.facebook.com/groups/SalemMutualAidGroup/</a><br>
+  Email:  <a href="mailto:salemvillagecarms@yahoo.co.uk">salemvillagecarms@yahoo.co.uk</a><br>
+  Phone  - 01558 822875 for Karen and Andrew Goss / 07891 592428 for Community  Councillor Owen Williams<br>
+  </p>
+</div>
 
-  <details>
-    <summary>Business & Finance</summary> 
-      <?php include("covid/business.php"); ?>
-  </details>
+<div class="bb mb5">
+  <h3>Business  and Funding</h3>
+  <p>Find  information about financial and other support available for  businesses and employers from the following websites:</p>
+  
+  <ul class="link-list">
+    <li><a href="https://gov.wales/business-and-employers-coronavirus">Welsh  Government</a></li>
+    <li><a href="https://businesswales.gov.wales/coronavirus-advice/?_ga=2.75846166.2016518919.1604926842-647359841.1597735084">Business  Wales</a></li>
+    <li><a href="https://communityfoundationwales.org.uk/">Community  Foundation Wales</a></li>
+    <li><a href="https://www.carmarthenshire.gov.wales/home/business/">Carmarthenshire  County Council</a></li>
+  </ul>
+</div>
 
-  <details>
-    <summary>Education & Families</summary> 
-      <?php include("covid/education.php"); ?>
-  </details>
 
-  <details>
-    <summary>Health Information</summary> 
-      <?php include("covid/health.php"); ?>
-  </details>
+<div class="bb mb5">
+  <h3>Education  and Families</h3>
+  <h4>Welsh  Government</h4>
+  <p>Find  general information about <a href="https://gov.wales/guidance-learning-schools-and-settings-autumn-term-covid-19">learning  in schools</a>, <a href="https://gov.wales/find-out-about-free-school-meals-during-coronavirus-outbreak">free  school meals</a>,  and <a href="https://gov.wales/education-coronavirus">Coronavirus  regulations and restrictions</a>.</p>
+  
+  <h4>Carmarthenshire  County Council</h4>
+  <p>Find  local information about confirmed <a href="https://newsroom.carmarthenshire.gov.wales/2020/10/kcs-covid19/schools-covid-19-confirmed-cases/">Coronavirus  cases in schools</a>, <a href="https://hwb.gov.wales/distance-learning">distance  learning</a> and <a href="https://www.carmarthenshire.gov.wales/home/council-services/education-schools/">other  information</a>.</p>
+  
+  <h4>Domestic  Abuse</h4>
+  <p>The <a href="https://gov.wales/live-fear-free">Live  Fear Free Helpline</a> provides free help and advice about violence against women, domestic  abuse and sexual violence. Call, text or email any time.</p>
+</div>
 
-  <details>
-    <summary>Housing</summary> 
-      <?php include("covid/housing.php"); ?>
-  </details>
- </div>
 
+<div class="bb mb5">
+<h3>Health  and Wellbeing</h3>
+<h4>NHS</h4>
+<p>Find  information about <a href="https://www.nhs.uk/conditions/coronavirus-covid-19/">Coronavirus  symptoms</a>, <a href="https://www.nhs.uk/conditions/stress-anxiety-depression/">mental  health</a> and <a href="https://www.nhs.uk/">other  medical issues</a>.</p>
+
+<h4>Hywel  Dda University Health Board</h4>
+<p>Find  information about accessing <a href="https://hduhb.nhs.wales/healthcare/urgent-and-out-of-hours/">urgent  care</a>, <a href="https://hduhb.nhs.wales/healthcare/">non-emergency  care</a> and <a href="https://hduhb.nhs.wales/patient-information/">hospital  visits</a>,  and other <a href="https://hduhb.nhs.wales/">local  services</a>.</p>
+<p>
+</p>
+<h4>Mental  Health</h4>
+<p>Find  advice and support for mental health at the following websites:</p>
+
+<ul class="link-list">
+  <li><a href="https://www.callhelpline.org.uk/">C.A.L.L.  Mental Health Helpline for Wales</a></li>
+  <li><a href="https://www.meiccymru.org/">MEIC  Cymru</a></li>
+  <li><a href="https://www.mind.org.uk/about-us/mind-cymru/">MIND  Cymru</a></li>
+  <li><a href="https://www.moodjuice.scot.nhs.uk/">Moodjuice</a></li>
+  <li><a href="https://www.samaritans.org/?nation=wales">Samaritans</a></li>
+  <li><a href="http://www.wwamh.org.uk/">West  Wales Action for Mental Health</a></li>
+</ul>
+
+
+
+  <h4>Dentists</h4>
+  <p>Find  advice and information about dental issues from the following local  dental service websites:</p>
+  
+  <ul class="link-list">
+    <li><a href="http://www.celticdental.co.uk/">Deintyddfa  Celtic Dental Practice</a></li>
+    <li><a href="https://www.llandeilodentists.co.uk/en/">Llandeilo  Dental Practice</a></li>
+    <li><a href="http://www.llandeilodentist.co.uk/">Tywi  Dental Practice</a></li>
+  </ul>
+  
+  <h4>Doctors</h4>
+  <p>Find  advice and information on all health matters from your local surgery  at <a href="http://www.wales.nhs.uk/sites3/home.cfm?orgid=848">Meddygfa  Teilo</a>.</p>
+  
+  <h4>Pharmacies</h4>
+  <p>Find  advice and information on health, prescriptions and opening hours at <a href="https://nigelwilliamspharmacy.com/home">Nigel  Williams Pharmacy</a> and <a href="https://www.well.co.uk/">Well  Pharmacy</a> in Llandeilo.</p>
+</div>
 
 </main>
 
@@ -85,8 +148,6 @@ $page = basename($_SERVER["PHP_SELF"]);
 </footer>
 
 </div>
-
-<script src="js/scripts.min.js"></script>
 
 </body>
 </html>	
